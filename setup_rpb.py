5db74fb9e7233109f3c4549b72bc9c6768bc24fb
cisir4 <balandongiv@gmail.com>
2022-07-13 22:14:30 +0800

    Initial commit

---
+++ setup_rpb.py
@@ -0,0 +1,8 @@
+"""
+
+git clone https://github.com/clovaai/CRAFT-pytorch.git
+git https://github.com/clovaai/deep-text-recognition-benchmark
+https://towardsdatascience.com/pytorch-scene-text-detection-and-recognition-by-craft-and-a-four-stage-network-ec814d39db05
+
+
+"""
